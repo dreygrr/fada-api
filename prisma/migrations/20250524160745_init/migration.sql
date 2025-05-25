567b7371a656fbde1b2e8f367250735ad9548b2b
andrey de assis <85853286+dreygrr@users.noreply.github.com>
2025-05-25 20:27:22 -0300

    Create migration.sql

---
+++ prisma/migrations/20250524160745_init/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `user`;
+
+-- CreateTable
+CREATE TABLE `Usuario` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `nome` VARCHAR(191) NOT NULL,
+    `senha` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Usuario_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
